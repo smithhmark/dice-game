08965b3d30ea8984582354724a94ff71c764e18c
Mark Smith <smith.h.mark@gmail.com>
2016-01-29 16:05:01 -0800

    building a random board

--- src/Main.hs
+++ src/Main.hs
@@ -12,13 +12,15 @@ type Board = [Cell]
 
 type Rnd a = Rand StdGen a
 
---randCell :: RandomGen -> Int -> Int -> Rnd Cell
 randCell :: Int -> Int -> Rnd Cell
 randCell p d = do
   i <- getRandomR (0, p) :: Rnd Int
   c <- getRandomR (0, d) :: Rnd Int
   return $ Cell i c
 
+randBoard :: Int -> Int -> Int -> Rnd (Vector Cell)
+randBoard l p d = V.replicateM l $randCell p d
+  
 main :: IO ()
 main = do
   g <- getStdGen
@@ -26,4 +28,7 @@ main = do
   --print $ take 10 (randoms g :: [Double])
   aCell <- evalRandIO $ randCell 2 3
   print aCell
+
+  brd <- evalRandIO $ randBoard 4 2 3
+  print brd
   putStrLn "hello world"
