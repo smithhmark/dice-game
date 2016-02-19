7d10909fc606c523f7750a89a87f587f5144f90a
Mark Smith <smith.h.mark@gmail.com>
2016-02-19 13:03:34 -0800

    Using a hardcoded testcase

--- stresser/Main.hs
+++ stresser/Main.hs
@@ -10,6 +10,17 @@ main = do
   let sz = 3
       d = 3
       gs = buildGS 2 sz d
-  brd <- generateBoard gs
+  --brd <- generateBoard gs
+  --print brd
+  let brd = [Cell {owner = 0, dice = 3}
+            ,Cell {owner = 0, dice = 2}
+            ,Cell {owner = 0, dice = 2}
+            ,Cell {owner = 0, dice = 2}
+            ,Cell {owner = 1, dice = 1}
+            ,Cell {owner = 1, dice = 3}
+            ,Cell {owner = 0, dice = 1}
+            ,Cell {owner = 1, dice = 3}
+            ,Cell {owner = 1, dice = 3}
+            ]
   let startingTree = runReader (buildTree brd 0 0 True Nothing) gs
   cpuVsCpu gs startingTree
